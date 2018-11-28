411957ed2cd9d5316c5f8dca69bf3da398d36443
Nicholas Petrik <frokrab@gmail.com>
2018-11-27 19:05:15 -0500

    Added PostgreSQL helper functions and example config file

---
+++ database/config.example.js
@@ -0,0 +1,14 @@
+// Example PostgreSQL client config
+//
+// Please make a copy of this file in the same folder named config.js
+// and replace the values with your own values credentials.
+// 5432 is the default port number for PostgreSQL.
+//
+// database/config.js is already in the .gitignore file for you.
+
+module.exports = {
+  user: 'YOUR_USERNAME',
+  database: 'YOUR_DATABASE_NAME',
+  host: 'YOUR_HOSTNAME',
+  port: 5432,
+};
---
+++ database/pgdb.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const config = require('./config');
+
+const client = new Pool(config);
+
+const saveNote = (req, res) => {
+  console.log(req.body.input);
+  client
+    .query(`INSERT INTO dmi_notes.notes (note) VALUES ($1);`, [req.body.input])
+    .then(() => res.sendStatus(201))
+    .catch(err => res.status(500).send(err));
+};
+
+const getNotes = (req, res) => {
+  client
+    .query(`SELECT * FROM dmi_notes.notes ORDER BY id DESC;`)
+    .then(data => res.send(data))
+    .catch(() => res.sendStatus(500));
+};
+
+module.exports = {
+  saveNote,
+  getNotes,
+};
