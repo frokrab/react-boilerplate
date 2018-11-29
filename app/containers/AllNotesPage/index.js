b7f108a809ba0964f54e5b8da39ba40d8d1ba5ce
Nicholas Petrik <frokrab@gmail.com>
2018-11-29 18:35:52 -0500

    Added AllNotesPage and corresponding route

---
+++ app/containers/AllNotesPage/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+/* eslint-disable react/prefer-stateless-function */
+export default class AllNotesPage extends React.PureComponent {
+  render() {
+    return <div>Hey hello this is a div</div>;
+  }
+}
