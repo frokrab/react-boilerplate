781b72c081fe45028debad1fbadce962bfed5a79
Nicholas Petrik <frokrab@gmail.com>
2018-11-29 18:59:25 -0500

    Added NotesListEntry component to AddNotesPage

---
+++ app/containers/AllNotesPage/NotesListEntry.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const NotesListEntry = styled.li`
+  border: 1px solid;
+  padding-left: 1%;
+`;
+
+export default NotesListEntry;
--- app/containers/AllNotesPage/index.js
+++ app/containers/AllNotesPage/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
+
 import NotesList from './NotesList';
+import NotesListEntry from './NotesListEntry';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class AllNotesPage extends React.PureComponent {
   render() {
     return (
       <NotesList>
-        <li>this is where a note should go</li>
+        <NotesListEntry>
+          {JSON.stringify("here's my first note")}
+        </NotesListEntry>
+        <NotesListEntry>
+          {JSON.stringify('and this would be my second')}
+        </NotesListEntry>
+        <NotesListEntry>
+          {JSON.stringify('this should be a third one right here')}
+        </NotesListEntry>
       </NotesList>
     );
   }
