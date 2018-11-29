8f33f7b300504db37e5f6661b8028013ac36c442
Nicholas Petrik <frokrab@gmail.com>
2018-11-28 19:45:26 -0500

    Added constants for SAVE_NOTE action type

--- app/containers/HomePage/constants.js
+++ app/containers/HomePage/constants.js
@@ -3,3 +3,6 @@ export const CHANGE_INPUT = 'notes/Home/CHANGE_INPUT';
 export const GET_NOTES = 'notes/Home/GET_NOTES';
 export const GET_NOTES_SUCCESS = 'notes/Home/GET_NOTES_SUCCESS';
 export const GET_NOTES_FAILURE = 'notes/Home/GET_NOTES_FAILURE';
+export const SAVE_NOTE = 'notes/Home/SAVE_NOTE';
+export const SAVE_NOTE_SUCCESS = 'notes/Home/SAVE_NOTE_SUCCESS';
+export const SAVE_NOTE_FAILURE = 'notes/Home/SAVE_NOTE_FAILURE';
