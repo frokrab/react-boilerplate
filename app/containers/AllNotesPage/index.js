9a8a5eae87754900f7d9f82e77c05df6d4130c25
Nicholas Petrik <frokrab@gmail.com>
2018-11-29 19:41:18 -0500

    Added get notes functionality to AllNotesPage

--- app/containers/AllNotesPage/index.js
+++ app/containers/AllNotesPage/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import PropTypes from 'prop-types';
 
+import injectReducer from 'utils/injectReducer';
+import injectSaga from 'utils/injectSaga';
 import NotesList from './NotesList';
 import NotesListEntry from './NotesListEntry';
+import { getNotes } from './actions';
+import {
+  makeSelectNotes,
+  makeSelectLoading,
+  makeSelectError,
+} from './selectors';
+import reducer from './reducer';
+import saga from './saga';
 
 /* eslint-disable react/prefer-stateless-function */
-export default class AllNotesPage extends React.PureComponent {
+export class AllNotesPage extends React.PureComponent {
+  componentDidMount() {
+    this.props.getNotes();
+  }
+
   render() {
     return (
       <NotesList>
@@ -21,3 +39,32 @@ export default class AllNotesPage extends React.PureComponent {
     );
   }
 }
+
+AllNotesPage.propTypes = {
+  getNotes: PropTypes.func,
+};
+
+const mapDispatchToProps = dispatch => ({
+  getNotes: () => dispatch(getNotes()),
+});
+
+const mapStateToProps = createStructuredSelector({
+  notes: makeSelectNotes(),
+  loading: makeSelectLoading(),
+  error: makeSelectError(),
+});
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+const withSaga = injectSaga({ key: 'allNotes', saga });
+
+const withReducer = injectReducer({ key: 'allNotes', reducer });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(AllNotesPage);
