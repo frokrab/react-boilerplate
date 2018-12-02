244361dd3330741933001da36bea965bf2caa2fe
Nicholas Petrik <frokrab@gmail.com>
2018-12-02 14:57:38 -0500

    Added asynchronous loading for AllNotesPage

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -11,7 +11,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
-import AllNotesPage from 'containers/AllNotesPage/index';
+import AllNotesPage from 'containers/AllNotesPage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
 import GlobalStyle from '../../global-styles';
